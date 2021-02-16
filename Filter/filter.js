1c77c1b9c16264aa95f3ae04ebadb5168334db44
thob-js <79045367+thob-js@users.noreply.github.com>
2021-02-16 15:37:32 +0100

    final_

--- Filter/filter.js
+++ Filter/filter.js
@@ -1,8 +1,14 @@
 const outputSum = document.querySelector("#outputSum");
+const outputStock = document.querySelector("#outputStock");
 const tBody = document.querySelector("#tableOutput");
 
-var alleBestillinger = [];
+function round(thisNumber) {
+    let rounded = Math.round((thisNumber + Number.EPSILON) * 100) / 100;
+    return rounded;
+}
+
 var totalOmsetning = 0;
+var nettoTotalOmsetning = 0;
 var bestillingNr = 0;
 
 var stock = {
@@ -10,7 +16,9 @@ var stock = {
     polse: 20,
     liten_drikke: 20,
     stor_drikke: 20,
-    pommes: 20
+    pommes: 20,
+    ost: 20,
+    bacon: 20
 };
 
 var price = {
@@ -18,74 +26,139 @@ var price = {
     polse: 30,
     liten_drikke: 25,
     stor_drikke: 35,
-    pommes: 31
+    pommes: 31,
+    ost: 5,
+    bacon: 10,
+    'ost,bacon': 15,
 };
 
 class Bestilling{
-    constructor(_rett,_drikke,_side,_nr,_price){
+    constructor(_rett,_drikke,_side,_nr,_ekstra,_price){
         this.nr = _nr;
         this.rett = _rett;
         this.drikke = _drikke;
         this.side = _side;
+        this.ekstra = _ekstra;
+
         this.pris = this.orderCost;
+
         this.updateStock();
         this.print();
+        this.printStock();
+        this.printRevenue();
     }
 
     get orderCost() {
-        let costOfOrder = (price[this.rett] + price[this.drikke] + price[this.side]);
+        let costOfOrder = (price[this.rett] + price[this.drikke]);
+        if(this.side != 'ingen') {
+            costOfOrder += (price[this.side]);
+        }
+        if(this.ekstra != 'ingen') {
+            costOfOrder += (price[this.ekstra]);
+        }
         totalOmsetning += costOfOrder;
-        console.log("Bestillingen koster: "+costOfOrder);
         return costOfOrder;
     }
 
     updateStock() {
         stock[this.rett]--;
         stock[this.drikke]--;
-        stock[this.side]--;
+        if (this.side != 'ingen') {
+            stock[this.side]--;
+            this.stockCheckAndAlert(this.side);
+        }
+        this.stockCheckAndAlert(this.rett);
+        this.stockCheckAndAlert(this.drikke);
+    }
 
-        if(stock[this.drikke]<10) {
-            window.alert("lav stock av "+this.drikke);
+    stockCheckAndAlert(item) {
+        if (stock[item] < 10) {
+            window.alert("lav stock av "+item);
         }
-        if(stock[this.rett]<10) {
-            window.alert("lav stock av "+this.rett);
-        }
-        if(stock[this.side]<10) {
-            window.alert("lav stock av "+this.side);
-        }
-
-        console.log(stock[this.rett]);
-        console.log(stock[this.drikke]);
-        console.log(stock[this.side]);
     }
 
     print() {
+        let mva = this.pris / 100*15;
         tBody.innerHTML += `<tr>
             <td> ${this.nr} </td>
             <td> ${this.drikke} </td>
             <td> ${this.rett} </td>
             <td> ${this.side} </td>
             <td> ${this.pris},- </td>
-            <td> ${this.pris/100*15},- </td>
+            <td> ${round(mva)},- </td>
+            <td> ${this.ekstra},- </td>
         </tr>\n`;
+    }
+
+    printStock() {
+        let stockValues = JSON.stringify(stock);    // gjør om et objekt til string og sender til html
+        outputStock.innerHTML = stockValues;
+    }
+
+    printRevenue() {
+        let mva = totalOmsetning / 100*15;
+        let netto = totalOmsetning - mva;
+        outputSum.innerHTML = (
+            "Brutto omsetning = "+totalOmsetning+"kr."
+            +"<br> Hvorav mva. på 15% = ~"+round(mva)+"kr"
+            +"<br> Netto omsetning = "+netto+"kr"
+        );
+        nettoTotalOmsetning += netto;
     }
 }
 
-function newOrder(rett,drikke,side) {
-    bestillingNr++;
-    let nyBestilling = new Bestilling(rett,drikke,side,bestillingNr);
-    alleBestillinger.push(nyBestilling);
+const submit_btn = document.querySelector("#submit"); 
+submit_btn.addEventListener('click', function() {    //onclick get values fra brukervalg
+    let mat = "";
+    let drikke = "";
+    let side = "";
+    let ekstra = "";
 
-    console.log("ny: ",nyBestilling);
-    console.log("alle: ",alleBestillinger);
-    console.log("omsetning: ",totalOmsetning);
+    try {
+        mat = getSelectedRadio('mat');
+        drikke = getSelectedRadio('drikke');
+
+        try {
+            side = getSelectedRadio('side');
+        } catch (error) {
+            side = "ingen";
+        }
+
+        try {
+            ekstra = getSelectedCheckboxValues('ekstra');
+        } catch (error) {
+            ekstra = "ingen";
+        }
+
+        newOrder(mat,drikke,side,ekstra);
+    } catch (error) {
+        window.alert("du må minimum velge drikke og mat for å bestille en meny");
+    }
+});
+
+function getSelectedRadio(name) {    //finn value av én selected name="name", både radio og checkbox
+    const radio = document.querySelector(`input[name="${name}"]:checked`);
+    let value = radio.value;
+    return value;
 }
 
-const outputStock = document.querySelector("#outputStock");
-window.addEventListener('load', function fnLoad() {
-    newOrder('hamburger','stor_drikke','pommes');
-    newOrder('polse','liten_drikke','pommes');
+function getSelectedCheckboxValues(name) {   //alle checkbox values av name="name"
+    const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+    return values;
+}
 
-    let stockValues = JSON.stringify(stock);
-    outputStock.innerHTML = "Varelager:"+stockValues;
+function newOrder(rett,drikke,side,ekstra) {   //lag nytt objekt med variabler som input
+    bestillingNr++;
+    let nyBestilling = new Bestilling(rett,drikke,side,bestillingNr,ekstra);
+    console.log("ny: ",nyBestilling);
+}
+
+window.addEventListener('load', function() {    //testbestillinger
+    newOrder('hamburger','stor_drikke','pommes','ingen');
+    newOrder('polse','liten_drikke','pommes','ost,bacon');
+    newOrder('polse','liten_drikke','ingen','ost');
 });
